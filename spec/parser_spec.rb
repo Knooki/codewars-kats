35eadd1160ce4dd717ee8d3bd97b237690c3bae5
knooki <knukknakiknak@gmail.com>
2022-10-24 21:35:23 +0300

    naming of spec files fixed

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,7 +4,16 @@ require_relative '../parser'
 require_relative 'spec_helper'
 
 RSpec.describe Parser do
+<<<<<<< HEAD
   context 'given string array' do
     it { expect(described_class.new(['1.1', '2.2', '3.3']).call).to eq([1.1, 2.2, 3.3]) }
+=======
+  let(:parser) do
+    Parser.new(['1.1', '2.2', '3.3'])
+  end
+
+  context 'given string array' do
+    it { expect(parser.call).to eq([1.1, 2.2, 3.3]) }
+>>>>>>> naming of spec files fixed
   end
 end
