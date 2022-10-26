71a0b70a385261d122df923d9ebbce512f0d65ba
knooki <knukknakiknak@gmail.com>
2022-10-26 22:04:29 +0300

    changed specs

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,11 +4,7 @@ require_relative '../parser'
 require_relative 'spec_helper'
 
 RSpec.describe Parser do
-  let(:parser) do
-    Parser.new(['1.1', '2.2', '3.3'])
-  end
-
   context 'given string array' do
-    it { expect(parser.call).to eq([1.1, 2.2, 3.3]) }
+    it { expect(described_class.new(['1.1', '2.2', '3.3']).call).to eq([1.1, 2.2, 3.3]) }
   end
 end
