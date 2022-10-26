c3b9a6022a6d21e95034e9207a24c96b4bfb813a
knooki <knukknakiknak@gmail.com>
2022-10-26 21:58:44 +0300

    Done

--- count_the_smiley_faces.rb
+++ count_the_smiley_faces.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class CountTheSmileyFaces
+<<<<<<< HEAD
   attr_reader :given_arr
+=======
+  attr_reader :arr
+>>>>>>> Done
 
   def initialize(given_arr)
     @given_arr = given_arr
--- spec/count_the_smiley_faces_spec.rb
+++ spec/count_the_smiley_faces_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe CountTheSmileyFaces do
   context 'given string array' do
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     it { expect(described_class.new([':D', ':~)', ';~D', 'x)', ':O']).call).to eq(3) }
 =======
     it { expect(described_class.new([':D', ':~)', ';~D', ':)', ':O']).call).to eq(4) }
@@ -13,5 +14,8 @@ RSpec.describe CountTheSmileyFaces do
 =======
     it { expect(described_class.new([':D', ':~)', ';~D', 'x)', ':O']).call).to eq(3) }
 >>>>>>> naming fixed
+=======
+    it { expect(described_class.new([':D', ':~)', ';~D', ':)', ':O']).call).to eq(4) }
+>>>>>>> Done
   end
 end
