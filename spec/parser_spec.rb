c8d34f2beb16b6737febdf590c6b421f859ed84d
knooki <knukknakiknak@gmail.com>
2022-10-26 22:04:29 +0300

    changed specs

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -8,12 +8,8 @@ RSpec.describe Parser do
   context 'given string array' do
     it { expect(described_class.new(['1.1', '2.2', '3.3']).call).to eq([1.1, 2.2, 3.3]) }
 =======
-  let(:parser) do
-    Parser.new(['1.1', '2.2', '3.3'])
-  end
-
   context 'given string array' do
-    it { expect(parser.call).to eq([1.1, 2.2, 3.3]) }
+    it { expect(described_class.new(['1.1', '2.2', '3.3']).call).to eq([1.1, 2.2, 3.3]) }
 >>>>>>> naming of spec files fixed
   end
 end
