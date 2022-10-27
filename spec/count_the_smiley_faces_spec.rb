906bba99430ef52c2a540ad6887c93a7082e3c81
knooki <knukknakiknak@gmail.com>
2022-10-27 04:13:40 +0300

    naming fixed

--- spec/count_the_smiley_faces_spec.rb
+++ spec/count_the_smiley_faces_spec.rb
@@ -5,9 +5,13 @@ require_relative '../count_the_smiley_faces'
 RSpec.describe CountTheSmileyFaces do
   context 'given string array' do
 <<<<<<< HEAD
+<<<<<<< HEAD
     it { expect(described_class.new([':D', ':~)', ';~D', 'x)', ':O']).call).to eq(3) }
 =======
     it { expect(described_class.new([':D', ':~)', ';~D', ':)', ':O']).call).to eq(4) }
 >>>>>>> Done
+=======
+    it { expect(described_class.new([':D', ':~)', ';~D', 'x)', ':O']).call).to eq(3) }
+>>>>>>> naming fixed
   end
 end
