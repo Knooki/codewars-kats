711c731576bcfaf2cacd381b253ef2690a1c7a9d
knooki <knukknakiknak@gmail.com>
2022-10-28 02:27:31 +0300

    Done

---
+++ rotate_an_array_matrix.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class RotateAnArrayMatrix
+  attr_reader :matrix, :direction
+
+  def initialize(matrix, direction)
+    @matrix = matrix
+    @direction = direction
+  end
+
+  def call
+    result_mtrx = Array.new(@matrix[0].size) { Array.new(@matrix.size) }
+    @matrix.each_with_index do |row, i|
+      row.each_with_index do |el, j|
+        @direction == 'clockwise' ? result_mtrx[j][-(i + 1)] = el : result_mtrx[-(j + 1)][i] = el
+      end
+    end
+    result_mtrx
+  end
+end
---
+++ spec/rotate_an_array_matrix_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../rotate_an_array_matrix'
+require_relative 'spec_helper'
+
+RSpec.describe RotateAnArrayMatrix do
+  context 'when given square matrix and clockwise direction' do
+    let(:matrix) do
+      [[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9]]
+    end
+    it { expect(described_class.new(matrix, 'clockwise').call).to eq([[7, 4, 1], [8, 5, 2], [9, 6, 3]]) }
+  end
+  context 'when given 3x4 matrix and counter-clockwise direction' do
+    let(:matrix) do
+      [[1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [9, 10, 11, 12]]
+    end
+    it {
+      expect(described_class.new(matrix,
+                                 'counter-clockwise').call).to eq([[4, 8, 12], [3, 7, 11], [2, 6, 10], [1, 5, 9]])
+    }
+  end
+end
