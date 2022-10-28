31e26f35b0e15d96db75d5ea252408fd2ed5746a
knooki <knukknakiknak@gmail.com>
2022-10-28 04:59:47 +0300

    Done

---
+++ spec/product_of_consecutive_fib_numbers_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../product_of_consecutive_fib_numbers'
+require_relative 'spec_helper'
+
+RSpec.describe ProducOfConsecutiveFibNumbers do
+  context 'when invalid product is given' do
+    it { expect(described_class.new(2345).call).to eq([55, 89, false]) }
+  end
+  context 'when valid product is given' do
+    it { expect(described_class.new(602_070).call).to eq([610, 987, true]) }
+  end
+end
